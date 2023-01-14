b18f36c0d617a7ff346de2692486f9f37d99d56e
220825213 <220825213@tut4life.ac.za>
2023-01-14 11:13:59 +0200

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../style/app_style.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppStyle.mainColor,
+      appBar: AppBar(
+        elevation: 0.0,
+        title: Text("My notes"),
+        centerTitle: true,
+        backgroundColor: AppStyle.mainColor,
+      ),
+    );
+  }
+}
